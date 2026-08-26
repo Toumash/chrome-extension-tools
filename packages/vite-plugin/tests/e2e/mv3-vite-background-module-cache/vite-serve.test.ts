2c8ea60447bab859daa9a1f1c12825ce2dfec24a
Tomasz Dłuski <mr.toumash@gmail.com>
2026-08-26 10:07:40 +0200

    test: remove invalid fs-extra copy option

--- packages/vite-plugin/tests/e2e/mv3-vite-background-module-cache/vite-serve.test.ts
+++ packages/vite-plugin/tests/e2e/mv3-vite-background-module-cache/vite-serve.test.ts
@@ -26,7 +26,7 @@ test('background dependency update does not duplicate service worker modules', a
   const src2 = path.join(__dirname, 'src2')
 
   await fs.remove(src)
-  await fs.copy(src1, src, { recursive: true })
+  await fs.copy(src1, src)
 
   const { browser, routes } = await serve(__dirname)
   const page = await browser.newPage()
